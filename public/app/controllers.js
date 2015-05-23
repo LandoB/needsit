2f01b9f215eec5ff1f08b508837ccd763df3f7c5
Rolando Bernal <rolandobernal@Rolandos-MacBook-Pro.local>
2015-05-23 14:12:06 -0500

    Finished functionallity

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -3,3 +3,16 @@ var controllers = angular.module('myapp.controllers', []);
 controllers.controller('IndexController', ['$scope', function($scope){
   $scope.message = 'Hello I am the HOME PAGE';
 }])
+
+controllers.controller('AboutController', ['$scope', function($scope){
+  $scope.message = 'Hello I am the ABOUT PAGE';
+}])
+
+controllers.controller('NeedsController', ['$scope', function($scope){
+  $scope.message = 'Hello I am the NEEDS PAGE';
+  $scope.needs=[];
+  $scope.addNeeds=function(thingy){
+  	$scope.needs.push(thingy);
+  	$scope.newNeed='';
+  };
+}])
